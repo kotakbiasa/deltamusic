e5b8816cf7dba7d23cb82c05407fd49f286a19b1
Kotak Biasa <muhnurfauzan73@gmail.com>
2025-12-21 09:18:02 +0800

    feat: Add automatic tracking for groups and users interacting with the bot.

--- delta/plugins/core/tracker.py
+++ delta/plugins/core/tracker.py
@@ -8,23 +8,6 @@ from pyrogram import filters, types
 from delta import app, db
 
 
-@app.on_my_chat_member(filters.group)
-async def track_new_chats(_, m: types.ChatMemberUpdated):
-    """Track when bot is added to or removed from groups."""
-    new_status = m.new_chat_member.status if m.new_chat_member else None
-    old_status = m.old_chat_member.status if m.old_chat_member else None
-    
-    # Bot was added to group (became member/admin)
-    if new_status in ["member", "administrator"] and old_status in [None, "left", "kicked"]:
-        if not await db.is_chat(m.chat.id):
-            await db.add_chat(m.chat.id)
-    
-    # Bot was removed from group
-    elif new_status in ["left", "kicked"] and old_status in ["member", "administrator"]:
-        if await db.is_chat(m.chat.id):
-            await db.rm_chat(m.chat.id)
-
-
 @app.on_message(filters.group, group=1)
 async def track_groups(_, m: types.Message):
     """Auto-track all groups where bot receives messages."""
